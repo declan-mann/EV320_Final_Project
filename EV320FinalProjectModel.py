495f7d87d08adbea0f1118feb220a0843bb55c67
declan-mann <declanrmann@gmail.com>
2024-12-13 09:04:53 -0700

    Final Model Code
    This code is able to produce 5 figures. 2 for the Initial Conditions, 2 for the final elevations, and one figure showing the difference between the initial and final elevations.

--- EV320FinalProjectModel.py
+++ EV320FinalProjectModel.py
@@ -14,12 +14,17 @@ import sys
 # import .asc data
 ascii_grid = np.loadtxt("trailelevations.asc", dtype = 'float', skiprows=6)
 ascii_headers = np.loadtxt("trailelevations.asc", max_rows = 6, dtype = 'str')
-ascii_grid[ascii_grid==-9999]= np.nan
-n_long = ascii_headers[0,1].astype(int)
-n_lat = ascii_headers[1,1].astype(int)
+ascii_grid[ascii_grid==-9999]= np.nan        # This excludes all points without reliable data. like an elevation of -9999
+ascii_grid = ascii_grid[5:-2, 2:-2]
+
+# n_long = ascii_headers[0,1].astype(int)
+# n_lat = ascii_headers[1,1].astype(int)
+
+n_lat, n_long = ascii_grid.shape
 dxy = ascii_headers[4,1].astype(float)
 xllcorner = ascii_headers[2,1].astype(float)
 yllcorner = ascii_headers[3,1].astype(float)
+
 
 
 ## ---- GRID FORMATION
@@ -29,13 +34,13 @@ LAT, LONG = np.meshgrid(x, y, indexing='ij') # this sets up a plotting grid
 nodes = n_long*n_lat 
 
 ##-- Creating the lengths of the model 
-nx= 87 
-ny= 99
+nx= n_lat 
+ny= n_long
 
 dx=1 
 dy=1
 ## ---- TIME STEP
-dt = 10 # year
+dt = 10 # years (sjould be 10 )
 
 
 ## ---- DIFFUSION
@@ -61,11 +66,12 @@ fig = plt.figure()
 ax= fig.add_subplot(111, projection= '3d')
 ax.plot_surface(LAT,LONG, elv_matrix)
 
-ax.set_xlabel('Distance in meters')
-ax.set_ylabel('Distance in meters')
-ax.set_zlabel('Elevation in meters')
+ax.set_xlabel('Distance (m)')
+ax.set_ylabel('Distance (m)')
+ax.set_zlabel('Elevation (m)')
 
-## stability check 
+
+## Stability Check 
 sx= dt * D / dx**2 
 sy= dt * D / dy**2 
 
@@ -83,7 +89,7 @@ if sy > 0.5:
 
 ### ---------- A MATRIX AND RUNNING THE MODEL
 
-A = np.zeros((nx*ny, nx*ny)) # no longer use nodes bc 2 dimension
+A = np.zeros((nx*ny, nx*ny)) # no longer use nodes because 2 dimensions
 
 for i in range(nx):
     for k in range(ny):
@@ -97,6 +103,14 @@ for i in range(nx):
             A[ik,ik]= 1
         elif k == (ny-1):
             A[ik,ik]= 1
+        elif i == (nx-2):
+             A[ik,ik]= 1
+        elif i == (ny-2):
+             A[ik,ik]= 1
+        elif k == 1:
+           A[ik,ik]= 1
+        elif i == 1:
+            A[ik,ik]= 1
         else: 
             #-------- matrix coefficients 
             A[ik,ik]= 1-2*sx - 2*sy
@@ -104,6 +118,7 @@ for i in range(nx):
             A[ik, (i-1) * ny + k]= sx
             A[ik, i * ny + k + 1]= sy
             A[ik, i * ny + k - 1]= sy
+
 
 print(A)
 
@@ -113,21 +128,33 @@ print(A)
 totaltime= 1000
 time=0
 
+# A_m = np.ma.array(A, mask=np.isnan(A))
+# elv_flat_m = np.ma.array(elv_flat, mask=np.isnan(elv_flat))
+### CHANGE the maksing 
+A_m = np.ma.masked_invalid(A)  # Mask invalid (NaN or Inf) values in matrix A
+elv_flat_m = np.ma.masked_invalid(elv_flat)  # Mask invalid values in elevation data
+
+print(np.any(np.isnan(A_m)))  # Check for NaNs in A_m
+print(np.any(np.isnan(elv_flat_m)))  # Check for NaNs in elv_flat_m
+
 while time<=totaltime:
-    newz = np.dot(A,elv_flat)
-    elv_flat[:]= newz 
+    newz = np.ma.dot(A_m,elv_flat_m)
+    elv_flat_m[:]= newz 
     time += dt 
     
+# if np.any(np.isnan(elv_flat)):
+#     print("Initial elevation array contains NaN values.")
+#     sys.exit()
+    
 ### ----- RUN A QUCK CHANGE -----
-elv_matrix += np.random.random((LAT.shape))*5 # 5 meter random additions
+#elv_matrix += np.random.random((LAT.shape))*5 # 5 meter random additions
 ### ---- SAVE ASCII OUTPUT (this can be opened in qgis easily!) -----
 header = 'NCOLS %s \n' % n_long + 'NROWS %s \n' % n_lat + 'xllcorner %s \n' % xllcorner+ 'yllcorner %s \n' % yllcorner + 'cellsize %s \n' % dxy + 'NODATA_value - 9999'
 np.savetxt('new_elev.asc', elv_matrix, header = header, comments = '')
 
 
-
 # method 1 - use a surface plot 
-Z= elv_matrix.reshape(LAT.shape)
+Z= elv_flat_m.reshape(LAT.shape)
 fig = plt.figure()
 ax= fig.add_subplot(111, projection= '3d')
 ax.plot_surface(LAT,LONG,Z)
@@ -139,10 +166,22 @@ ax.set_zlabel('Elevation (m)')
 # method 2 --- uses pcolormesh
 
 fig2,ax2= plt.subplots(1,1)
-cbar1= ax2.pcolormesh(LAT,LONG,Z)
+cbar1= ax2.pcolormesh(LONG, LAT,Z)
 ax2.set_title('Final Elevations')
 
 fig2.colorbar(cbar1, ax=ax2, label='Elevation (m)')
+
+## Difference 
+elv_diff = Z- elv_matrix
+
+# Method 1: Surface plot for elevation difference
+fig_diff = plt.figure()
+ax_diff = fig_diff.add_subplot(111, projection='3d')
+ax_diff.plot_surface(LAT, LONG, elv_diff, cmap='RdBu', edgecolor='none')
+ax_diff.set_xlabel('Distance (m)')
+ax_diff.set_ylabel('Distance (m)')
+ax_diff.set_zlabel('Elevation Difference (m)')
+ax_diff.set_title('Elevation Difference (Final - Initial)')
 
 
 
