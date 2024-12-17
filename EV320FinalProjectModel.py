6eb41da1467fbcabd862ae2d77a8fe69d7085425
declan-mann <declanrmann@gmail.com>
2024-12-17 14:15:51 -0700

    Final Running Annotated Code

--- EV320FinalProjectModel.py
+++ EV320FinalProjectModel.py
@@ -6,47 +6,52 @@ Created on Thu Dec 12 10:23:12 2024
 @author: declanmann
 """
 """
+This information can also be found in the README file on GitHub
 --------------------
-My Earth Science Question: How do hiking trails affect hillslope diffusion over time? How long does it take for the 
-hiking trails to be affected by hillslope diffusion? 
+My Earth Science Question:
+    
+    How do hiking trails affect hillslope diffusion over time? How are hiking trails affected by 
+    hillslope diffusion?
 
-This model is a 2 dimensional diffsion problem
+    This model is a 2-dimensional diffusion problem
 --------------------
 INPUTS:
-    
-The research area used for this model was sourced from Lidar data to collect elevations. The research area
-in this code is located in Eden, Utah along the South Skyline Trail. This trail cuts through part of the Uinta-Wasatch-Cache 
-National Forest. The size of the research area is 99 meters by 87 meters, but the model only uses a 80 by 95 meter space of the
-area. This is due to the lidar data having some NaN values so I trimmed the data grid, and the appearance of the final figures
-is much cleaner. 
+        The research area used for this model was sourced from Lidar data to collect elevations. The research area
+    imported to this code is located in Eden, Utah, along the South Skyline Trail. The space chosen for this model
+    has 2 parts of a hiking trail included. One hiking trail cuts around the high elevations on the hillslope, 
+    and the other cuts pretty much straight across the lower hillslope. The trails cut through part of the 
+    Uinta-Wasatch-Cache National Forest. On the plots, you can identify the trails as they appear a darker color than
+    the rest of the hillslope. There is a valley/dip in the elevation at the lower end plot, but you can tell it is 
+    not a trail because there is a sharp increase in elevation after the dark blue area. The research area is 99 
+    meters by 87 meters, but the model only uses an 80 by 95-meter space of the area. This is due to the Lidar data
+    having some NaN values, so I trimmed the data grid, and the appearance of the final figures is much cleaner. 
 
-The elevations data file can be found in the GitHub repository. A google earth file outlining the research area can also be found there
+    The elevation data file can be found in the GitHub repository and it is titled "trailelevations.asc". A Google 
+    Earth file outlining the research area can also be found in the repository titled "EV320.kml." The Google Earth 
+    file allows you to explore the search area in an accessible way if you don't have a computer that can handle more 
+    complex GIS systems. 
 
-This model runs over the course of 250 years and uses a diffusion coefficient of 0.004 m2/yr. This model also has a time step of
-10 years. 
+    This model runs for 250 years and uses a diffusion coefficient of 0.004 m2/yr. This model also has a time step of 
+    10 years. 
 --------------------
 OUTPUTS: 
     Two 3D surface plots with a slope gradient colormap overlayed with these titles-
-    
-        1. Initial Elevations of the South Skyline Trail in Eden, UT
-        2. Elevations on the South Skyline Trail in Eden, Utah after 250 Years
-        
-    One surface plot depicitng the difference in elevations titled:
-        
-        1. Difference in Elevation After Diffusion (Final - Initial)
-    
-    Saves two text files in .asc format for exporting data
+        1. "Initial Elevations of the South Skyline Trail in Eden, UT"
+        2. "Elevations on the South Skyline Trail in Eden, Utah after 250 Years"
+    One surface plot depicting the difference in elevations is titled:   
+        1. "Difference in Elevation After Diffusion (Final - Initial)"
+    The code will also save two text files in .asc format for exporting data, like a 3d print, or to collect elevations 
+    for multiple different run times. 
 --------------------
-Note: There are other lines of code to plot a colormesh figure depictiing the elevations. They are not used for the final outputs in
-this model, but can be used for future use, if someone decides to use this code a template. 
+Note: 
 
+    There are other lines of code to plot a color mesh figure depicting the elevations. They are not used for the 
+    final outputs in this model but can be used for future use if someone decides to use this code as a template.
 """
-
 ### -------- IMPORT MODULES ---------
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
 
 ### -------- INITIAL CONDITIONS -------------
     # import .asc (Elevation) data 
@@ -56,13 +61,11 @@ ascii_headers = np.loadtxt("trailelevations.asc", max_rows = 6, dtype = 'str')
 ascii_grid[ascii_grid==-9999]= np.nan        # This excludes all points without reliable data. like an elevation of -9999
 ascii_grid = ascii_grid[5:-2, 2:-2]          # Here I trim the ascii grid to get rid of NaN values as most of them were focused around the edges/corners
 
-
 # Define some grid parameters 
 n_lat, n_long = ascii_grid.shape             # Number of rows/colunmns values 
 dxy = ascii_headers[4,1].astype(float)       # The size of each cell 
 xllcorner = ascii_headers[2,1].astype(float)      
 yllcorner = ascii_headers[3,1].astype(float)        # define the coordinates of the corners
-
 
 ## ---- GRID FORMATION
 x = np.arange(0, dxy*n_lat, dxy) + xllcorner  # array of x values corresponding to cells on the grid
@@ -84,13 +87,12 @@ dt = 10 # years
 # Diffusion Coefficient
 D = 0.004 # m2/year    
 
-
 ## ---- INITIAL ELEVATION
 elv_flat = ascii_grid.flatten()     # this flattens my 2D elevation grid into a 1D array 
 
 
 elv_matrix = elv_flat.reshape(LONG.shape) # elevation (z) is currently a 1D, 
-# but we will reshape it into the 2D coordinate form - recognizing that we want it to follow theshape of X.
+# but we will reshape it into the 2D coordinate form - recognizing that we want it to follow the shape of X.
 
 ### --------- PLOT INITIAL CONDITIONS -------------
 
@@ -102,12 +104,10 @@ elv_matrix = elv_flat.reshape(LONG.shape) # elevation (z) is currently a 1D,
     # ax.set_ylabel('Distance (m)')
     # ax.set_title('Initial conditions')
 
-
 # create a gradient function to show slope of hillslope in plot
 Z_x, Z_y= np.gradient(elv_matrix, dxy, dxy)     # this computes the gradient in both the x and y direction. 
-
-slope = np.sqrt(Z_x**2 + Z_y**2)           # calculate slope
-slope_normalized = slope / np.max(slope)      # Normalizes the slope values make visualization easier
+slope = np.sqrt(Z_x**2 + Z_y**2)                # calculate slope
+slope_normalized = slope / np.max(slope)        # Normalizes the slope values make visualization easier
 
 # Create a 3D surface plot 
 fig = plt.figure()
@@ -125,7 +125,6 @@ fig.colorbar(mappable, ax=ax, shrink=0.5, aspect=5, label='Slope Magnitude')
 
 #----------------------
 ## Stability Check (Courant Condition)
-
 sx= dt * D / dx**2      # stability x-direction
 sy= dt * D / dy**2      # stability y direction
 
@@ -143,9 +142,7 @@ if sy > 0.5:
     print('unstable y') 
     sys.exit()
 
-
 ### ---------- A MATRIX -----------------
-
 A = np.zeros((nx*ny, nx*ny)) # no longer use nodes because 2 dimensions
 
 for i in range(nx):
@@ -175,12 +172,9 @@ for i in range(nx):
             A[ik, (i-1) * ny + k]= sx
             A[ik, i * ny + k + 1]= sy
             A[ik, i * ny + k - 1]= sy
-
-# print(A)
-
+# You can print(A) if you believe the matrix is not correct
 
 ###------- Time loop -----------
-
 totaltime= 250    #250 year run time 
 time=0
 
@@ -197,8 +191,6 @@ while time<=totaltime:
     newz = np.ma.dot(A_m,elv_flat_m)
     elv_flat_m[:]= newz 
     time += dt 
-    
-    
 
 # Create a slope gradient for the final plot 
 Z= elv_flat_m.reshape(LAT.shape)   #final elevation variable
@@ -210,7 +202,6 @@ final_slope= np.sqrt(Zx**2 + Zy**2) # calculate the slope
 final_slope_normalized = final_slope / np.max(final_slope)      # scales the slope values make visualization easier
 
 ## method 1 - use a surface plot 
-
 fig = plt.figure()
 ax= fig.add_subplot(111, projection= '3d')
 ax.plot_surface(LAT,LONG,Z, facecolors=plt.cm.viridis(final_slope_normalized), rstride=1, cstride=1) # overlays the final slopes gradient on top of the surface plot
@@ -219,27 +210,22 @@ ax.set_ylabel('Distance (m)')
 ax.set_zlabel('Elevation (m)')
 ax.set_title('Elevations on the South Skyline Trail in Eden, Utah after 250 Years', fontsize= '15')
 
-
 # add color bar for final plot to show slope magnitude
 mappable = plt.cm.ScalarMappable(cmap='viridis', norm=plt.Normalize(vmin=0, vmax=np.max(final_slope)))
 mappable.set_array(final_slope)
 fig.colorbar(mappable, ax=ax, shrink=0.5, aspect=5, label='Slope Magnitude')
 
-
 # Colormesh not used in final outputs 
-    # method 2 --- uses pcolormesh
-
+  # method 2 --- uses pcolormesh
     # fig2,ax2= plt.subplots(1,1)
     # cbar1= ax2.pcolormesh(LONG, LAT,Z)
     # ax2.set_title('Final Elevations after 1000 years on Lewis Peak and the South Skyline Trail in Huntsville, UT')
     
     # fig2.colorbar(cbar1, ax=ax2, label='Elevation (m)')
 
-## Crearte a Difference Plot, to emphasize changes in elevation over the run time
-
+## Create a Difference Plot, to emphasize changes in elevation over the run time
 elv_diff = Z- elv_matrix # calculate the difference between the final and initial elevations 
 
-# use a surface plot because 2D diffusion
 fig_diff = plt.figure()
 ax_diff = fig_diff.add_subplot(111, projection='3d')
 ax_diff.plot_surface(LAT, LONG, elv_diff, cmap='RdBu', edgecolor='none')
@@ -248,12 +234,7 @@ ax_diff.set_ylabel('Distance (m)')
 ax_diff.set_zlabel('Elevation Difference (m)')
 ax_diff.set_title('Difference in Elevation After Diffusion (Final - Initial)', fontsize= '15')
 
-
 ### ---- SAVE ASCII OUTPUT for further data usage -----
 header = 'NCOLS %s \n' % n_long + 'NROWS %s \n' % n_lat + 'xllcorner %s \n' % xllcorner+ 'yllcorner %s \n' % yllcorner + 'cellsize %s \n' % dxy + 'NODATA_value - 9999'
 np.savetxt('Final_elev.asc', Z, header = header, comments = '')
 np.savetxt('Init_elev.asc', elv_matrix, header=header, comments='')
-
-
-
-
